aab2eedbdafb718dc7257323af7342394ed8403f
otaviofriedein <friedein.otavio@gmail.com>
2025-05-27 22:28:48 -0300

    Adicionando validação de DataAnnotations

--- Models/Noticia.cs
+++ Models/Noticia.cs
@@ -1,13 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace tag_news.Models
 {
     public class Noticia
     {
         public int Id { get; set; }
-        public string Titulo { get; set; }
-        public string Texto { get; set; }
-        public int UsuarioId { get; set; }
 
-        public Usuario Usuario { get; set; }
-        public ICollection<NoticiaTag> NoticiaTags { get; set; }
+        [StringLength(250, MinimumLength = 3, ErrorMessage = "O campo deve ter entre 3 e 250 caracteres.")]
+        [Required]
+        public string Titulo { get; set; }
+
+        [StringLength(800, MinimumLength = 3, ErrorMessage = "O campo deve ter entre 3 e 800 caracteres.")]
+        [Required]
+        public string Texto { get; set; }
+        public int? UsuarioId { get; set; }
+
+        public Usuario? Usuario { get; set; }
+        public ICollection<NoticiaTag>? NoticiaTags { get; set; }
     }
 }
--- Models/Tag.cs
+++ Models/Tag.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace tag_news.Models
 {
     public class Tag
     {
         public int Id { get; set; }
+
+        [StringLength(100, MinimumLength = 3,ErrorMessage = "O campo deve ter entre 3 e 100 caracteres.")]
+        [Required]
         public string Descricao { get; set; }
 
-        public ICollection<NoticiaTag> NoticiaTags { get; set; }
+        public ICollection<NoticiaTag>? NoticiaTags { get; set; }
     }
 }
