c61886dba65d614bc799c1fd7f87a8ec39df175c
otaviofriedein <friedein.otavio@gmail.com>
2025-05-27 23:59:20 -0300

    Adicionando validação do frontend via ajax

--- Models/Noticia.cs
+++ Models/Noticia.cs
@@ -1,17 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace tag_news.Models
 {
     public class Noticia
     {
-        public int Id { get; set; }
-
-        [StringLength(250, MinimumLength = 3, ErrorMessage = "O campo deve ter entre 3 e 250 caracteres.")]
-        [Required]
-        public string Titulo { get; set; }
-
-        [StringLength(800, MinimumLength = 3, ErrorMessage = "O campo deve ter entre 3 e 800 caracteres.")]
-        [Required]
+        public int Id { get; set; }       
+        public string Titulo { get; set; }       
         public string Texto { get; set; }
         public int? UsuarioId { get; set; }
 
--- ViewModels/NoticiaViewModel.cs
+++ ViewModels/NoticiaViewModel.cs
@@ -1,7 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 public class NoticiaViewModel
 {
     public int Id { get; set; }
+
+
+    [StringLength(250, MinimumLength = 3, ErrorMessage = "O campo Titulo deve ter entre 3 e 250 caracteres.")]
+    [Required(ErrorMessage = "Titulo é obrigatório")]
     public string Titulo { get; set; }
+
+    [StringLength(800, MinimumLength = 3, ErrorMessage = "O campo Texto deve ter entre 3 e 800 caracteres.")]
+    [Required(ErrorMessage = "Texto é obrigatório")]
     public string Texto { get; set; }
     public int? UsuarioId { get; set; }
     public List<int>? TagIds { get; set; } = [];
